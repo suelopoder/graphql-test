4c27678c4a9beeb5ac87925e03b56248f7cf157a
Diego Sisto <diego.sisto@xapo.com>
2020-05-08 17:39:52 -0300

    Add capital city

--- src/GeneralData.js
+++ src/GeneralData.js
@@ -6,11 +6,11 @@ const LangMap = {
   default: 'That\'s cool!',
 };
 
-const GeneralData = ({ name, population, languages, area, continent }) => (
+const GeneralData = ({ name, population, languages, area, continent, capital }) => (
   <section>
     <h2>Country of the day is {name}</h2>
     <p>It's population is {new Intl.NumberFormat().format(population)} peps. They live in {new Intl.NumberFormat().format(area)} km<sup>2</sup>. That's one {Math.ceil(population/area)} people per km<sup>2</sup>!</p>
-    <p>We are talking about {continent} and the majority speaks {languages[0]}. <b>{LangMap[languages[0]] || LangMap.default}</b></p>
+    <p>We are talking about {continent} and it's capital is {capital}. The majority speaks {languages[0]}. <b>{LangMap[languages[0]] || LangMap.default}</b></p>
   </section>
 );
 
